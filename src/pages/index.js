173e701c4fe8d5cb98b969c7d7b0b4452913028b
Oscar Franco <ospfranco@protonmail.com>
2020-05-02 11:21:36 +0200

    more love

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,22 +20,22 @@ const IndexPage = () => (
       <div style={{display: `flex`, flexDirection: `column`, justifyContent: `center`, alignItems: `center`, flex: 1, minWidth: '320px',}}>
         <img src={icon} style={{width: 100}}/>
         <img src={logo} style={{width: 150}}/>
-        <h1 style={{fontWeight: 200}}>Build status at a glance.</h1>
+        <h1 style={{fontWeight: 200, textAlign: 'center'}}>Build status at a glance.</h1>
       </div>
     </div>
-    <div style={{display: `flex`, flexWrap: "wrap", margin: `0 auto`, flex: 1, alignItems: 'center', justifyContent: 'center', marginBottom: 60}}>
+    <div style={{display: `flex`, flexWrap: "wrap-reverse", margin: `0 auto`, flex: 1, alignItems: 'center', justifyContent: 'center', marginBottom: 60}}>
       <div style={{display: `flex`, flexDirection: 'column', justifyContent: `center`, alignItems: `center`, flex: 1, minWidth: '320px', padding: 10}}>
-        <h2 style={{fontWeight: 200 }}>Sits on your desktop menu bar.</h2>
-        <h2 style={{fontWeight: 200 }}>Notifies you when a build breaks.</h2>
-        <h2 style={{fontWeight: 200 }}>Easy set up and integration.</h2>
-        <h2 style={{fontWeight: 200 }}>Trigger a rebuild with a single click.</h2>
+        <h2 style={{fontWeight: 200, textAlign: 'center'}}>Sits on your desktop menu bar.</h2>
+        <h2 style={{fontWeight: 200, textAlign: 'center'}}>Notifies you when a build breaks.</h2>
+        <h2 style={{fontWeight: 200, textAlign: 'center'}}>Easy set up and integration.</h2>
+        <h2 style={{fontWeight: 200, textAlign: 'center'}}>Trigger a rebuild with a single click.</h2>
       </div>
 
       <img src={background2} style={{height: '30vh', borderRadius: 10}}/>
     </div>
 
     <div style={{display: `flex`, flexDirection: `column`, margin: `0 auto`, flex: 1, alignItems: 'center', padding: 10}}>
-      <h2 style={{fontWeight: 200 }}>Multiple CI providers supported</h2>
+      <h2 style={{fontWeight: 200, textAlign: 'center'}}>Multiple CI providers supported</h2>
       <div style={{display: `flex`, justifyContent: `center`, alignItems: `center`, flex: 1, justifyContent: `space-around`, width: '300px'}}>
         <div style={{display: `flex`, flexDirection: `column`, alignItems: 'center' }}>
         
