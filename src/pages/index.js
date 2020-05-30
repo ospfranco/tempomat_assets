5b5c0531b756adfcbbf3ba45352e0ffebfeb79f2
Oscar Franco <ospfranco@protonmail.com>
2020-05-30 15:43:18 +0200

    Update page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import screenshot from '../images/screenshot.jpg'
+import screenshot from '../images/screenshot.png'
 import screenshotDark from '../images/screenshotDark.jpg'
 import background2 from '../images/background2.jpg'
 import circleci from '../images/circleci_dark.png'
@@ -12,7 +12,7 @@ import travis from '../images/travis_dark.png'
 import github from '../images/github_dark.png'
 import macButton from '../images/DownloadMac.svg'
 import logo from '../images/title.png';
-import icon from '../images/Icon.png';
+import icon from '../images/PngIcon.png';
 
 const IndexPage = () => (
   <Layout>
@@ -26,13 +26,13 @@ const IndexPage = () => (
           <img src={icon} style={{width: 100, height: 100, marginRight: 40}} alt="Tempomat Logo"/>
           <div >
             <img src={logo} style={{width: 150, marginBottom: 0}} alt="Tempomat Name"/>
-            <h1 style={{fontWeight: 200}}>Build Status in your Menu Bar</h1>
+            <h1 style={{fontWeight: 200}}>Build Status Everywhere</h1>
           </div>
         </div>
       </div>
-      <div className="laptop">
+
         <img src={screenshot} />
-      </div>
+
     </div>
 
     <div style={{display: `flex`, flexDirection: `column`, justifyContent: `center`, alignItems: `center`}}>
@@ -63,10 +63,10 @@ const IndexPage = () => (
 
     <div style={{display: `flex`, flexWrap: "wrap-reverse", margin: `0 auto`, flex: 1, alignItems: 'center', justifyContent: 'center', marginBottom: 60}}>
       <div style={{display: `flex`, flexDirection: 'column', justifyContent: `center`, alignItems: `center`, flex: 1, minWidth: '320px', padding: 10}}>
-        <h2 style={{fontWeight: 200, textAlign: 'center'}}>Sits on your desktop menu bar</h2>
+        <h2 style={{fontWeight: 200, textAlign: 'center'}}>Available on macOS/iOS/iPadOS</h2>
         <h2 style={{fontWeight: 200, textAlign: 'center'}}>Notifies you when a build breaks</h2>
         <h2 style={{fontWeight: 200, textAlign: 'center'}}>Easy set up and integration</h2>
-        <h2 style={{fontWeight: 200, textAlign: 'center'}}>Trigger a rebuild with a single click</h2>
+        <h2 style={{fontWeight: 200, textAlign: 'center'}}>Open Source</h2>
       </div>
 
       <img src={background2} style={{height: '30vh'}} alt="popover view"/>
@@ -84,7 +84,7 @@ const IndexPage = () => (
     
     <div style={{display: `flex`, margin: `0 auto`, flex: 1, justifyContent: 'center', padding: 40}}>
       <a href="https://apps.apple.com/de/app/tempomat-circleci-toolbar/id1509296762?l=en&mt=12" alt="Mac app store link">
-        <img src={macButton} style={{marginBottom: 0, width: 400}}/>
+        <img src={macButton} style={{marginBottom: 0, width: 300}}/>
       </a>
     </div>
   </Layout>
