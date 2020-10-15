ecdd3e4aa009f51b82d1ca770f7a30cce5c0cdd4
Oscar Franco <ospfranco@protonmail.com>
2020-10-15 23:30:03 +0200

    More style

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-import Steps from "../images/Steps.png";
+// import Steps from "../images/Steps.png";
+import StepsVertical from "../images/steps_vertical.png";
+import StepsHorizontal from "../images/steps_horizontal.png";
 import TitleLight from "../images/titleLight.png";
 import macButton from "../images/DownloadMac.svg";
 import explanation from "../images/explanation.png";
 import notification from "../images/notification.png";
 import providers from "../images/providers.png";
-import macbook from "../images/macbook.png";
+import abstractPreview from "../images/abstractPreview.png"
 
 const IndexPage = () => (
   <Layout>
@@ -19,21 +21,25 @@ const IndexPage = () => (
     <div className="topSection">
       <img src={TitleLight} style={{ width: 200 }} />
 
-      <img src={Steps} className="abstractBanner" />
+      {/* <img src={Steps} className="abstractBanner" /> */}
+      <picture>
+        <source media="(max-width: 799px)" srcset={StepsVertical}/>
+        <img src={StepsHorizontal} className="abstractBanner"/>
+      </picture>
+
 
       <div
         style={{
           alignSelf: "center",
           padding: 20,
-          // textAlign: "center",
           maxWidth: 700,
-          color: 'white'
+          // color: 'white'
         }}
       >
-        <p style={{fontSize: 36, fontWeight: '600'}}>Monitor your builds, from your desktop.</p>
-        <p style={{fontSize: 20, lineHeight: '1.5em'}}>
+        <p style={{fontSize: 36, fontWeight: '600', color: 'white'}}>Monitor your builds, from your desktop.</p>
+        <p style={{fontSize: 20, lineHeight: '1.5em', color: 'white'}}>
           Tempomat sits on the macOS menu bar and retrieves information from your
-          CI/CD system, access your builds immediately and stop wasting your time.
+          CI/CD system, stop wasting your time.
         </p>
       </div>
 
@@ -44,37 +50,44 @@ const IndexPage = () => (
           href="https://apps.apple.com/de/app/tempomat-circleci-toolbar/id1509296762?l=en&mt=12"
           alt="Mac app store link"
         >
-          <img src={macButton} style={{ marginBottom: 0, width: 160, marginRight: 30 }} />
+          <img src={macButton} style={{ marginBottom: 0, width: 160}} />
         </a>
-
+        <div style={{width: 30}}/>
         <a
           href="#features"
           alt="Mac app store link"
           className="button"
         >
-          See the features
+          Show features
         </a>
       </div>
     </div>
 
     <div className="midSection" id="features">
-      <div style={{marginTop: 80, marginBottom: 80, display: 'flex', justifyContent: 'center'}}>
-        {/* <p style={{fontSize: 24, fontWeight: 600, color: '#05BEA4'}}>Why Tempomat... why not?!</p> */}
-        <p style={{fontSize: 28, maxWidth: 700, fontWeight: 500}}>All your builds... from all your branches... from all your CIs... neatly presented, sitting comfortably on the menu bar, accesible with a single click. <br/><br/>Don't pretend you are not <span style={{fontWeight: 'bold', color: '#05BEA4'}}>excited</span>.</p>
+      <div style={{display: 'flex', flexWrap: 'wrap', alignItems: 'center', marginTop: 40, marginBottom: 100, justifyContent: 'center'}}>
+        <img src={abstractPreview} className="mainAbstract" />
+        <div style={{ padding: 40, flex: 1 }}>
+          <p style={{fontSize: 24}}>All your builds,<br/>from all your branches,<br/> from all your CIs,<br/>organized and sitting comfortably on the menu bar, accesible with a single click.</p>
+          <p style={{color: '#D33374'}}>Are you excited already?</p>
+        </div>
       </div>
-      <div style={{borderBottom: '1px solid #CCC'}}/>
+      
+
+      <div style={{borderBottom: '1px solid #CCC', width: '100%'}}/>
 
       <div style={{
         display: 'flex',
         flexWrap: 'wrap',
+        width: '100%',
         marginTop: 80,
-        marginBottom: 80
+        marginBottom: 80,
+        justifyContent: 'space-around'
       }}>
         <div className="section">
           <img src={explanation} className="explanation" />
           <p style={{fontWeight: 600, fontSize: 20}}>Fast & Furious</p>
           <p style={{fontSize: 16}}>
-            Say good bye to the <span style={{fontStyle: 'italic'}}>slow</span> web, say hi to a <span style={{fontWeight: 'bold', color: '#05BEA4'}}>fast</span> native solution.
+            Say good bye to the <span style={{fontStyle: 'italic'}}>slow</span> web, say hi to a <span style={{color: '#D33374'}}>fast</span> native solution.
           </p>
         </div>
 
@@ -82,7 +95,7 @@ const IndexPage = () => (
           <img src={notification} className="explanation" />
           <p style={{fontWeight: 600, fontSize: 20}}>Out of the way</p>
           <p style={{fontSize: 16}}>
-            Receive <span style={{fontWeight: 'bold', color: '#05BEA4'}}>notifications</span> when any of your builds break.
+            Receive <span style={{color: '#D33374'}}>notifications</span> when any of your builds break.
           </p>
         </div>
 
@@ -90,44 +103,54 @@ const IndexPage = () => (
           <img src={providers} className="explanation" />
           <p style={{fontWeight: 600, fontSize: 20}}>Works for you</p>
           <p style={{fontSize: 16}}>
-          Supports many <span style={{fontWeight: 'bold', color: '#05BEA4'}}>CI providers</span> with more added regularly.
+          Supports many <span style={{color: '#D33374'}}>CI providers</span> with more added regularly.
           </p>
         </div>
       </div>
 
-      <div style={{borderBottom: '1px solid #CCC'}}/>
+      <div style={{borderBottom: '1px solid #CCC', width: '100%'}}/>
 
       <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 100, marginBottom: 100}}>
         <p style={{fontSize: 16}}>
         <h2>and that is not all...</h2>
           <ul>
-            <li>Not a SaaS, <span style={{fontWeight: 'bold', color: '#05BEA4'}}>buy it once</span>, then it's yours</li>
-            <li>Powerful <span style={{fontWeight: 'bold', color: '#05BEA4'}}>regex</span> filters</li>
-            <li><span style={{fontWeight: 'bold', color: '#05BEA4'}}>Securely</span> stores data on the macOS keychain</li>
-            <li><span style={{fontWeight: 'bold', color: '#05BEA4'}}>Beautiful</span> minimalistic design</li>
-            <li>Trigger a <span style={{fontWeight: 'bold', color: '#05BEA4'}}>rebuild</span> with a single click!</li>
+            <li>Not a subscription service, <span style={{color: '#D33374'}}>buy it once</span>, own it until the end of time.</li>
+            <li>Filter using your custom <span style={{color: '#D33374'}}>regexes</span>, even do invert filtering.</li>
+            <li>Everything is stored locally <span style={{color: '#D33374'}}>securely</span> on the keychain, no 3rd party snooping into your data.</li>
+            <li><span style={{color: '#D33374'}}>Beautiful</span> minimal design, no ads.</li>
+            <li>Trigger a <span style={{color: '#D33374'}}>rebuild job</span> with a single click, inside of the app!</li>
           </ul>
         </p>
       </div>
+    
+      <div style={{borderBottom: '1px solid #CCC', width: '100%'}}/>
+    
     </div>
-    <div className="bottomSection">
+    <div style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        width: '100%',
+        marginTop: 80,
+        marginBottom: 80,
+        justifyContent: 'space-around'
+      }}>
       <div className="section">
-        <h3 style={{ color: `white` }}>Must have for developers</h3>
-        <h1 style={{ color: `white` }}>5/5</h1>
-        <div style={{ color: `white` }}>
+        <h3 >Must have for developers</h3>
+        <h1 style={{color: '#57BEEE'}}>5/5</h1>
+        <div >
           One of those tools that you don't realize how convenient it makes life
           until you try it. Works great with CircleCI and saves time having to
           wait and check the build status manually.
         </div>
-        <h4 style={{ color: `white`, marginTop: 20 }}>by srolija</h4>
+        <h4 style={{  marginTop: 20, color: '#57BEEE' }}>by srolija</h4>
       </div>
 
       <div className="section">
-        <h3 style={{ color: `white` }}>
+        <h3 >
           A simple tool that adds so much DX value
         </h3>
-        <h1 style={{ color: `white` }}>5/5</h1>
-        <div style={{ color: `white` }}>
+        <h1 style={{color: '#57BEEE'}}>5/5</h1>
+        <div >
           I have no idea why a tool like this has not been published earlier.
           Having a unobtrusive reminder of CI status is extremely useful. While
           email or slack notifications might be great for important build
@@ -138,21 +161,22 @@ const IndexPage = () => (
           easily recommend to anyone using CI on your projects - especially
           CircleCI.
         </div>
-        <h4 style={{ color: `white`, marginTop: 20 }}>by twgraham</h4>
+        <h4 style={{  marginTop: 20, color: '#57BEEE' }}>by twgraham</h4>
       </div>
 
       <div className="section">
-        <h3 style={{ color: `white` }}>Useful little app</h3>
-        <h1 style={{ color: `white` }}>5/5</h1>
-        <div style={{ color: `white` }}>
+        <h3 >Useful little app</h3>
+        <h1 style={{color: '#57BEEE'}}>5/5</h1>
+        <div >
           Found it useful to track my builds, developer was very responsive and
           fixed a small bug I had immediately
         </div>
-        <h4 style={{ color: `white`, marginTop: 20 }}>by Speller</h4>
+        <h4 style={{  marginTop: 20, color: '#57BEEE' }}>by Speller</h4>
       </div>
 
+    </div>
       <div
-        className="section"
+
         style={{
           display: `flex`,
           flexDirection: `column`,
@@ -168,7 +192,6 @@ const IndexPage = () => (
           <img src={macButton} style={{ marginBottom: 0, width: 250 }} />
         </a>
       </div>
-    </div>
   </Layout>
 );
 
